876c854a2fc711513d22c6817786123e1ca76407
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-03-17 17:42:02 +0700

    API GET /items/{item_id}

--- path_params_numeric_validations/main.py
+++ path_params_numeric_validations/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from typing import Annotated
 
 app = FastAPI()
 
@@ -7,3 +8,18 @@ async def get_hello():
     return {
         "message": "Hello, World!"
     }
+
+@app.get("/items/{item_id}")
+async def get_items(
+    item_id: Annotated[int, Path()],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    result = {
+        "item_id": item_id
+    }
+    if q:
+        result.update({
+            "q": q
+        })
+
+    return result
